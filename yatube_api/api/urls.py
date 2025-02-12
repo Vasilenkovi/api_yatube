686361ba1fe650c610ea044ea2f6e97f6ad53cb6
ShizVizor <vladimir.vasilenko.0303@mail.ru>
2025-02-13 01:18:20 +0700

    Initial d

---
+++ yatube_api/api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
+from .views import PostViewSet, GroupViewSet, CommentViewSet
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'groups', GroupViewSet)
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('v1/posts/<int:post_id>/comments/', CommentViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('v1/posts/<int:post_id>/comments/<int:comment_id>/', CommentViewSet.as_view({
+        'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'
+    })),
+    path('v1/api-token-auth/', views.obtain_auth_token),
+]
---
+++ yatube_api/api/views.py
@@ -0,0 +1,44 @@
+from rest_framework import viewsets, permissions
+from rest_framework.exceptions import NotFound
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from posts.models import Post, Group, Comment
+from .serializers import PostSerializer, GroupSerializer, CommentSerializer
+from .permissions import IsAuthorOrReadOnly
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        if not Post.objects.filter(id=post_id).exists():
+            raise NotFound()
+        return Comment.objects.filter(post_id=post_id)
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs.get('post_id')
+        post = Post.objects.get(id=post_id)
+        serializer.save(author=self.request.user, post=post)
+
+    def get_object(self):
+        comment_id = self.kwargs.get('comment_id')
+        try:
+            comment = Comment.objects.get(id=comment_id, post__id=self.kwargs['post_id'])
+        except Comment.DoesNotExist:
+            raise NotFound()
+        self.check_object_permissions(self.request, comment)
+        return comment
